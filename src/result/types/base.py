233a078598d6dcee0ec8660c1f158a83e58c2678
williams ayo <williamsayo44@gmail.com>
2026-03-07 03:11:43 +0200

    Add type for result combine

--- src/result/types/base.py
+++ src/result/types/base.py
@@ -1,10 +1,8 @@
-from typing import List, Union
+from typing import List
 from result.base import _Ok, _Fail
-from result.nothing import NothingType
 
 type Either[S, F] = Ok[S] | Fail[F]
 type Ok[S] = _Ok[S]
 type Fail[F] = _Fail[F]
 type Result[T] = Ok[T] | Fail[T]
-type Option[T] = Either[T, NothingType]
 type ResultCombine[S, F] = Either[List[S], F]
